a7fa1800808506c09bfd22eab3c13bd0b9b55841
Hanok <hanok_k92@yahoo.co.in>
2015-01-19 02:21:23 +0530

    initialisation of the repo

---
+++ nearestneighbour.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	int places;
+	printf("Enter the total no of places");
+	scanf("%d",&places);
+	int a[places][places],i,j,hash[places];
+	printf("give the adjacency matrix\n");
+	for (i = 0; i < places; i++)
+	{
+		hash[i]=0;
+		for (j = 0; j < places; j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	hash[0]=1;
+	int min,next,k=0;
+	for (i = 1; i < places; i++)
+	{
+		min=99999;
+		for (j = 0; j < places; j++)
+		{
+			if (hash[j]==0 && j!=k && a[k][j]<min)
+			{
+				min=a[k][j];
+				next=j;
+				hash[j]=1;
+			}
+		}
+		printf("%d\n",next);
+	}
+	return 0;
+}
