d5374b413c043f938694b9955f9b0adf46f43395
Hanok <hanok_k92@yahoo.co.in>
2015-02-09 15:54:01 +0530

    modified

--- nearestneighbour.c
+++ nearestneighbour.c
@@ -6,25 +6,31 @@ int main(int argc, char const *argv[])
 	scanf("%d",&places);
 	int a[places][places],i,j,hash[places];
 	printf("give the adjacency matrix\n");
+	for(i=0;i<places;i++)
+	{
+
+		hash[i]=0;
+}
 	for (i = 0; i < places; i++)
 	{
-		hash[i]=0;
 		for (j = 0; j < places; j++)
 		{
 			scanf("%d",&a[i][j]);
 		}
 	}
 	hash[0]=1;
-	int min,next,k=0;
+	printf("0\n");
+	int min,next=0,k=0;
 	for (i = 1; i < places; i++)
 	{
 		min=99999;
 		for (j = 0; j < places; j++)
 		{
-			if (hash[j]==0 && j!=k && a[k][j]<min)
+			if ((hash[j]==0) && (a[k][j]!=0) && (a[k][j]<min))
 			{
 				min=a[k][j];
 				next=j;
+				k=j;
 				hash[j]=1;
 			}
 		}
